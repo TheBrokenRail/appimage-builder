0053167f68521998480876e32173c1b8f182597f
TheBrokenRail <connor24nolan@live.com>
2022-03-11 18:24:40 -0500

    Update Mime Database

--- appimagebuilder/modules/setup/generator.py
+++ appimagebuilder/modules/setup/generator.py
@@ -167,6 +167,7 @@ class RuntimeGenerator:
             helpers.OpenSSL,
             helpers.Python,
             helpers.Qt,
+            helpers.Mime,
         ]
 
         for helper in execution_list:
